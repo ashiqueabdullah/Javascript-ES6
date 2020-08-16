9310efb05acdead90906123e1b0bc8500f22ada0
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-16 20:21:41 +0600

    "Spread" Used to merge two arrays

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 "use strict"
 test();
 function test(){
-    name="Ashique Abdullah";
+  var a="Ashique Abdullah";
+  console.log(a);
 }
+
+
+let poorCountry=['Bangladesh','Maldip'];
+let richCountry=['Canada','USA'];
+
+let porrAndRich=[...poorCountry,...richCountry];
+console.log(porrAndRich);
