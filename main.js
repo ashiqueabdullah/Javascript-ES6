d5d27f5bef9379e8f5bb2eb0051a834ca09fabe0
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-17 22:06:42 +0600

    Object

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 
-//for of loop
+//object
 
-var poorCountry=['Bangladesh','Maldip','Srilonka'];
-for(let i of poorCountry){
-  console.log(i+" is poor country");
-}
+var car={
+  bmw:{
+    spreed:200,
+    color:"black",
+    model:2020
+  },
+  toyta:{
+    spreed:100,
+    color:"green",
+    model:2021
+  }
+};
+var cars={
+  spreed:200,
+  color:"red"
+};
+
+console.log(car['bmw']['color']);
+console.log(cars['color']);
+
 
 
 
