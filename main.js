eeb56a6c8c4356b2db87acaf28d2f1f1b0600077
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-17 19:55:06 +0600

    variable hoisting

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 
-//dynamic function
-var fun=function(n){
-  var sum=0;
-  for(var i=0; i<n; i++){
-    sum=sum+i;
-  }
-  return sum;
-}
-console.log(fun(10));
+//variable hoisting
+name="Ashique ABdullah";
+console.log(name);
+var name;
 
 
 
