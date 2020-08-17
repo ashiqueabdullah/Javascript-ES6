577a3c8927c1e4ad4a2df12529a568c8db6a06a0
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-17 23:37:06 +0600

    Function

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-//for in loop
-
-var car={spreed:100,color:"red",model:2020};
-
-for (let i in car){
-  console.log(i+"= "+car[i]);
+//simple function
+function simple(){
+  var x=10;
+  var y=20;
+  console.log(x+y)
 }
+simple();
+
+//parameterized function
+function para(a,b){
+  console.log(a+b);
+}
+para(10,20);
+
+//rest parameter function 
+function par(...x){
+  console.log(x);
+}
+par(1,2,3,4)
+
+// return function
+function retn(){
+  return "Ashique Abdullah"
+}
+
+function retn2(){
+  return "Name= "
+}
+console.log(retn2()+retn())
+
+//Anonymous function
+var name=function(nam){
+  console.log("My name is "+nam);
+}
+name("Ashique Abdullah")
+
+//Arrow function
+var testfucn=(value)=>{
+  console.log("Hello Hello "+value+" Hello")
+}
+testfucn("Arrow function testing");
