15e3385b2d92f2bd139a3e3e643a2ce81726024f
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-18 11:32:09 +0600

    multidimensional array

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-//Simple array
-var myar=['A','s','h','i','q','u','e']
-for(let i of myar){
-  console.log(i)
-}
+//multidimensional Array
+
+var one=[1,2,3]
+var two=[4,5,6]
+var three=[7,8,9]
+
+var all=[one,two,three]
+
+console.log(all[0])
