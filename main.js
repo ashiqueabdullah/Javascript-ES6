c009cfee9eb41d9b887f01be6ed8c8d5b2140997
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-18 21:09:12 +0600

    module

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-//inheritance
+import {phone} from '../mainTwo.js'
 
-class test{
-
-  prr(){
-    console.log("Hello testing")
-  }
-
-}
-
-class test2 extends test{
-  em(){
-    super.prr()
-  }
-}
-
-
-var obj=new test2();
-
-obj.em()
+console.log(phone)
---
+++ mainTwo.js
@@ -0,0 +1,3 @@
+const phone=['SMS','NOK']
+
+export {phone}
