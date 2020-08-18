b3ceb459baac15909f42ff44fec7b779b0ea522a
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-18 11:28:45 +0600

    Simple Array

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
-//simple function
-function simple(){
-  var x=10;
-  var y=20;
-  console.log(x+y)
+//Simple array
+var myar=['A','s','h','i','q','u','e']
+for(let i of myar){
+  console.log(i)
 }
-simple();
-
-//parameterized function
-function para(a,b){
-  console.log(a+b);
-}
-para(10,20);
-
-//rest parameter function 
-function par(...x){
-  console.log(x);
-}
-par(1,2,3,4)
-
-// return function
-function retn(){
-  return "Ashique Abdullah"
-}
-
-function retn2(){
-  return "Name= "
-}
-console.log(retn2()+retn())
-
-//Anonymous function
-var name=function(nam){
-  console.log("My name is "+nam);
-}
-name("Ashique Abdullah")
-
-//Arrow function
-var testfucn=(value)=>{
-  console.log("Hello Hello "+value+" Hello")
-}
-testfucn("Arrow function testing");
