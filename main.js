3e4ff17fb61bd124fe4186e17129bc6207426fba
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-18 13:35:12 +0600

    Class

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-//set
-var st= new Set()
+//class
 
-
-st.add("Names: ")
-st.add("Ashique Abdullah")
-
-//st.clear()
-//st.delete("Names: ")
-console.log(st)
-
-var st2=new Set([1,2,3,4])
-console.log(st2)
-console.log(st2.size)
-if(st2.has(1)){
-  console.log("yes")
-}else{
-  console.log("No")
+class test{
+  prt(){
+    console.log("Class testing")
+  }
 }
+
+var obj=new test()
+
+obj.prt()
